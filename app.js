5e1b67787168f1ad51fadd88512ecd412c46fded
Lennie Nurse <lnursej@gmail.com>
2023-04-22 17:16:38 -0400

    nothing

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const express = require("express");
 
-// CONFIGURATION
-const app = express();
-
-// ROUTES
-app.get("/", (req, res) => {
-  res.send("Welcome to Bookmarks App");
-});
-
-// EXPORT
-module.exports = app;
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
 DROP DATABASE IF EXISTS destinationchange;
 CREATE DATABASE destinationchange;
+
 \c destinationchange;
 
+DROP TABLE IF EXISTS  users;
 CREATE TABLE users (
   id INT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
@@ -9,9 +11,24 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL,
   region_id INT,
   about TEXT,
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   image TEXT
+);
+
+DROP TABLE  IF EXISTS region;
+CREATE TABLE region (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS projectdescription;
+CREATE TABLE projectdescription (
+  id INT PRIMARY KEY,
+  user_id INT NOT NULL,
+  content TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 -- CREATE TABLE likes (
@@ -21,17 +38,4 @@ CREATE TABLE users (
 --   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 --   FOREIGN KEY (user_id) REFERENCES users(id),
 --   FOREIGN KEY (post_id) REFERENCES posts(id)
--- );
-
-CREATE TABLE region (
-  id INT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE projectdescription (
-  id INT PRIMARY KEY,
-  user_id INT NOT NULL,
-  content TEXT,
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- )
