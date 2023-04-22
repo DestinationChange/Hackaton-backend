4baa981f7b70e82ed802bc743760c9e7d7f0eeb8
MarsiaRochelin <marsrochelin@pursuit.org>
2023-04-22 01:40:58 -0400

    db_config

---
+++ db_config.js
@@ -0,0 +1,23 @@
+const pgp = require("pg-promise")();
+require("dotenv").config();
+
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
+
+const db = pgp(cn);
+
+module.exports = db;
